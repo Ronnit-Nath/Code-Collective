8fe63c6bfe77d2c6baa9888c6142bff548bc895c
Ronnit Nath <ronnitnath03@gmail.com>
2023-04-17 02:58:42 +0530

    Connected Vision to the Interweb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,36 @@
 /** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  // ...
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
+
 module.exports = {
   content: ["*"],
   theme: {
     extend: {},
   },
-  variants:{
-    extend:{
-      display: ['group-focus'],
-      opacity: ['group-focus'],
-      inset:['group-focus']
+  variants: {
+    extend: {
+      display: ["group-focus"],
+      opacity: ["group-focus"],
+      inset: ["group-focus"],
     },
   },
   plugins: [],
-}
+};
+
+const plugin = require("tailwindcss/plugin");
+
+module.exports = {
+  plugins: [
+    plugin(function ({ addUtilities, addComponents, e, config }) {
+      // Add your custom styles here
+    }),
+  ],
+};
